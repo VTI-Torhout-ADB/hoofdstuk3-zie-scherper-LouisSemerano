70c7b3dcfad1978095bd1b2671ae07e1dc054ea0
Louis Semerano <semeranolouis@leerling.sint-rembert.be>
2025-02-05 10:00:52 +0100

    Systeem informatie

--- Systeem informatie/Program.cs
+++ Systeem informatie/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string UserName = Environment.UserName;
+            string MachineName = Environment.MachineName;
+            int ProcessorCount = Environment.ProcessorCount;
+            bool is64OperatingSystem = Environment.Is64BitOperatingSystem;
+            double WorkingSet = Environment.WorkingSet;
+            double WorkingSetMB = Math.Round(WorkingSet / 1000000 , 2);
+            double WorkingSetGB = Math.Round(WorkingSet / 1000000000 , 2);
+
+            Console.WriteLine($"Systeeminformatie voor {UserName} op {MachineName}:\r\n---------------------------------------------------\r\n    Aantal processors: {ProcessorCount}\r\n    64-bit besturingssysteem: {is64OperatingSystem}\r\n    Huidige geheugengebruik: {WorkingSetMB} MB ({WorkingSetGB} GB)\r\n---------------------------------------------------");
         }
     }
 }
