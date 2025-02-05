f55c16c17ac8656873393850482ae83e97789b95
Louis Semerano <semeranolouis@leerling.sint-rembert.be>
2025-02-05 10:03:42 +0100

    Mad Libs Dertien in een dozijn opnieuw maken want ik had verkeerde gemaakt

--- Dertien in een dozijn/Program.cs
+++ Dertien in een dozijn/Program.cs
@@ -4,18 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geef een naam:");
-            string woord1 = Console.ReadLine();
-            Console.WriteLine("Geef een zelfstandig naamwoord:");
-            string woord2 = Console.ReadLine();
-            Console.WriteLine("Geef een adjectief:");
-            string woord3 = Console.ReadLine();
-            Console.WriteLine("Geef een werkwoord:");
-            string woord4 = Console.ReadLine();
-
-            Console.WriteLine("\nHier komt het!");
-
-            Console.WriteLine($"Op een dag ging {woord1} naar de AP Hogeschool. Hij zag daar een {woord3} {woord2} en vond dat zo grappig dat hij begon te {woord4}.");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
