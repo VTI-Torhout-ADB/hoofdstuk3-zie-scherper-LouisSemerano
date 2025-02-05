eaf9ea50e2d1cf6869ca76a18fa8b956aeb76f93
Louis Semerano <semeranolouis@leerling.sint-rembert.be>
2025-02-05 10:07:37 +0100

    Update Dertien i een dozijn

--- Dertien in een dozijn/Program.cs
+++ Dertien in een dozijn/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int doosGrootte = 8;
+            int aantalEieren = 124;
+
+            Console.WriteLine("Hoeveel dozen heb je?:");
+            int aantalDozen = Console.ReadLine();
+
+
+
+            Console.WriteLine($"{aantalEieren} eieren passen in {aantalDozen} dozen van doosgrootte:{doosGrootte}. Daarbij zal je nog 4 eieren over hebben.");
         }
     }
 }
